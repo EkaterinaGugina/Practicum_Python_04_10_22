e8e0464cfcf9ffe68a8015744c53ce0e674e72cb
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-10-07 03:12:53 +0500

    created Ex4

--- HomeWork/Ex4_сoeff_polynom.py
+++ HomeWork/Ex4_сoeff_polynom.py
@@ -1,11 +1,57 @@
-# Ex4_koeff_polynom. Задана натуральная степень k. 
-# Сформировать случайным образом список коэффициентов 
+# Ex4_koeff_polynom. Задана натуральная степень k. Сформировать случайным образом список коэффициентов
 # (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+import random
+from unicodedata import name
 
-n = int(input('Введите натуральное число n = '))
-string_binary = ''
-res = n
-while res > 0:
-    string_binary += str(res % 2)
-    res //= 2
-print(f'число {n} в двоичной системе --> {string_binary[: : -1]}')
+def creat_polynom():
+    k = int(input('Введите степень многочлена (натуральное число, большее нуля), k = '))
+    polynom_coeff = [random.randint(0, 100) for i in range(k + 1)]
+    print(polynom_coeff)
+    if polynom_coeff[0] == 0:
+        polynom_coeff[0] = random.randint(1, 100)
+    if k != 1:
+        polynom_string = str(polynom_coeff[0])+'*x^' + str(k) + ' '
+        count = k - 1
+        while count > 1:
+            if polynom_coeff[k - count] != 0:
+                polynom_string += '+ ' + \
+                    str(polynom_coeff[k - count]) + \
+                    '*x^' + str(count) + ' '
+                count -= 1
+        if count == 1:
+            polynom_string += '+ ' + \
+                str(polynom_coeff[k - count]) + '*x  + ' + \
+                str(polynom_coeff[k - count + 1]) + ' = 0'
+    elif k == 1:
+        polynom_string = str(polynom_coeff[0]) + \
+            '*x + ' + str(polynom_coeff[1]) + ' = 0'
+    print(polynom_string)
+    return polynom_string
+
+f1 = creat_polynom()
+f2 = creat_polynom()
+
+with open('file_1.txt', 'w', encoding='utf-8') as f:
+    f.write(f1)
+f.close()
+with open('file_2.txt', 'w', encoding='utf-8') as f:
+    f.write(f2)
+f.close()
+   
+
+
+# def write_poly_file(name_file):
+#     with open('name_file.txt', 'w', encoding='utf-8') as f:
+#         return f.write(creat_polynom())
+#     f.close()
+# write_poly_file(file_1.txt)
+# write_poly_file('file_2')
+    
+
+# f1 = creat_polynom()
+# print(f1)
+# f2 = creat_polynom()
+# print(f2)
+# with open('file1.txt', 'w', encoding='utf-8') as f1:
+#     f1.write(polynom1)
+# f1.close()
