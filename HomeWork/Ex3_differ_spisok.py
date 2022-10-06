8b3d7d8a1ced7cec07bf7454ca20af37a77bac8a
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-10-07 00:54:31 +0500

    update Ex3

--- HomeWork/Ex3_differ_spisok.py
+++ HomeWork/Ex3_differ_spisok.py
@@ -1,21 +1,13 @@
-# Ex3_differ_spisok. Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+# Ex3_differ_spisok. Задайте последовательность чисел. 
+# Напишите программу, которая выведет список неповторяющихся 
+# элементов исходной последовательности.
 from random import randrange
 array = [randrange(1, 10) for i in range(int(input('Введите число элементов списка n = ')))]
-differ_spisok = []
+differ_elem = []
 print(f'{array},  неповторяющиеся элементы: ')
-for  elem in array:
-    if elem not in differ_spisok:
-        differ_spisok.append(elem)
-print(differ_spisok)
-    
-    
-    
-#     for  i in range(len(array)):
-#     kolvo = 0
-#     for j in range(len(array)):
-#         if array[j] == array[i]:
-#             kolvo += 1
-#     if kolvo == 1:
-#         differ_spisok.append(array[i])
-# print(differ_spisok)
-    
+for elem in array:
+        count_elem = array.count(elem)
+        if count_elem == 1:
+            differ_elem.append(elem)
+print(differ_elem)   
+  
